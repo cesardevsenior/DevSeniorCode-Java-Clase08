a377d4d5bbaa9fb3102ecc7c703fb5e93c014fda
Mary Joseph Causil Silva <majocausil16@gmail.com>
2024-11-20 22:16:18 -0500

    hola gente

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Aqui es donde está el problema
+
+        System.out.println("hello everybody :)");
+
     }
 }

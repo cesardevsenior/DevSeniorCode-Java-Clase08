2cdda145c347561419efe9ba7d5a30474a766efb
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2024-11-20 21:49:22 -0500

    Primer avance del proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ahora si es hora de trabajar!!");
     }
 }

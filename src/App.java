349245d9e1c01704a70638e401f7922e48c3c73a
sarayv9026 <sarayqvsao@gmail.com>
2024-11-20 22:00:32 -0500

    Nuevo comentario

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ahora si es hora de trabajar!!");
         System.out.println("Esta es una nueva version");
-
+        System.out.println("Commit prueba");
+        
         // todo: dev senior el future
 
         // HOLAAAAAAA MI NOMBRE ES MAJO XD

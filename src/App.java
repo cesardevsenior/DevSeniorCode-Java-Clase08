897b3d246bcb1756ad23e6aeccaffc69598fb680
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2024-11-20 21:53:12 -0500

    Agregada una nueva instruccion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ahora si es hora de trabajar!!");
+        System.out.println("Esta es una nueva version");
     }
 }
